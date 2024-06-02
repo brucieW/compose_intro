823b0774ea86a9e0bd0e5c57045d63fa2023367f
brucieW <bruciew50@gmail.com>
2024-06-02 10:08:56 +1000

    Minor changes.

--- app/src/main/java/com/zeroboss/composeintroduction/MainActivity.kt
+++ app/src/main/java/com/zeroboss/composeintroduction/MainActivity.kt
@@ -3,7 +3,6 @@ package com.zeroboss.composeintroduction
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
 import com.zeroboss.composeintroduction.ui.screens.home.HomeScreen
--- app/src/main/java/com/zeroboss/composeintroduction/ui/screens/lazy_row/LazyRowScreen.kt
+++ app/src/main/java/com/zeroboss/composeintroduction/ui/screens/lazy_row/LazyRowScreen.kt
@@ -1,8 +1,6 @@
 package com.zeroboss.composeintroduction.ui.screens.lazy_row
 
 import androidx.compose.animation.animateContentSize
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,7 +14,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -25,14 +22,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import com.zeroboss.composeintroduction.ui.common.ComputerLanguage
-import com.zeroboss.composeintroduction.ui.common.ComputerLanguage.Companion.getImage
 import com.zeroboss.composeintroduction.ui.common.Constants.selectedColor
 import com.zeroboss.composeintroduction.ui.common.LanguageTitle
 import com.zeroboss.composeintroduction.ui.theme.ComposeIntroductionTheme
